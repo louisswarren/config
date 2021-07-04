55ebb27ad6441a8f516486c0877418fc618134fa
Louis Warren <louis@lsw.nz>
2021-07-04 14:59:53 +1200

    Move colouring to py

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -2,3 +2,6 @@ config.load_autoconfig()
 
 with open("/home/louis/.config/base16-qutebrowser/themes/default/base16-gruvbox-dark-hard.config.py") as f:
     exec(f.read())
+
+c.colors.statusbar.private.bg = base02
+c.colors.statusbar.private.fg = base0E
