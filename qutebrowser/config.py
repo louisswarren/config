fb449ef95414f80de97659bc7817001500302102
Louis Warren <louis@lsw.nz>
2021-09-02 12:27:33 +1200

    Move qutebrowser fonts to config.py

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,3 +1,6 @@
+from platform import node as GETHOSTNAME
+HOST = GETHOSTNAME()
+
 config.load_autoconfig()
 
 with open("/home/louis/.config/base16-qutebrowser/themes/default/base16-gruvbox-dark-hard.config.py") as f:
@@ -5,3 +8,9 @@ with open("/home/louis/.config/base16-qutebrowser/themes/default/base16-gruvbox-
 
 c.colors.statusbar.private.bg = base02
 c.colors.statusbar.private.fg = base0E
+
+if HOST == 'turing':
+    c.fonts.default_size = '12pt'
+    c.fonts.web.size.default = 18
+    c.fonts.web.size.minimum = 16
+    c.qt.highdpi = True
