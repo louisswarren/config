ef24f1df74198c0cb25880b4a81426db36cb52a7
Louis Warren <louis@lsw.nz>
2022-04-04 21:51:46 +1200

    Distinguish selected tab better in qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -6,6 +6,10 @@ config.load_autoconfig()
 with open("/home/louis/.config/base16-qutebrowser/themes/default/base16-gruvbox-dark-hard.config.py") as f:
     exec(f.read())
 
+for seltab in (c.colors.tabs.selected.even, c.colors.tabs.selected.odd):
+    seltab.fg = base0D # base05
+    seltab.bg = base03 # base02
+
 c.colors.statusbar.private.bg = base02
 c.colors.statusbar.private.fg = base0E
 
