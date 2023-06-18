7e65fe91eb8ec903257b1c39771fce7deb38507f
Louis Warren <louis@lsw.nz>
2023-06-18 22:43:57 +1200

    Write a nice python startup

--- python/startup.py
+++ python/startup.py
@@ -1,10 +1,12 @@
-import atexit
-import os
-import readline
-
+################################################################################
+# Set up history
 # Can do away with this once
 # https://github.com/python/cpython/pull/13208
 # is merged (any day now ...)
+
+import atexit #Required
+import os #Required
+import readline #Required
 
 if 'PYTHONHISTORY' in os.environ:
     histfile = os.path.expanduser(os.environ['PYTHONHISTORY'])
@@ -25,3 +27,46 @@ def save(prev_h_len, histfile):
     readline.set_history_length(1000)
     readline.append_history_file(new_h_len - prev_h_len, histfile)
 atexit.register(save, h_len, histfile)
+
+
+################################################################################
+# Other libraries I want in my REPL
+
+import datetime
+import re
+import sys
+import itertools
+import pprint
+
+sys.displayhook = pprint.pprint
+
+################################################################################
+# Automatically save/load globals to/from shelf
+
+import atexit
+import shelve
+import xdg.BaseDirectory
+
+def _auto_shelve_globals(globals_initial):
+    keys = set(globals().keys()) - globals_initial
+    path = xdg.BaseDirectory.xdg_cache_home + '/python-interactive-shelf.db'
+    with shelve.open(path) as db:
+        for key in db:
+            del db[key]
+        for key in keys:
+            db[key] = globals()[key]
+        if keys:
+            print("Shelved", ", ".join(keys))
+
+def _auto_load_shelf():
+    path = xdg.BaseDirectory.xdg_cache_home + '/python-interactive-shelf.db'
+    with shelve.open(path) as db:
+        for key in db:
+            globals()[key] = db[key]
+        if db:
+            print("Loaded", ", ".join(db))
+
+atexit.register(_auto_shelve_globals, set(globals().keys()))
+_auto_load_shelf()
+del _auto_shelve_globals
+del _auto_load_shelf
